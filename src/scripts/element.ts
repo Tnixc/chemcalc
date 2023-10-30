8e23c11f2eb17da6cdfcbc737ed8d47d2f518326
Tnixc <tnixxc@gmail.com>
2023-10-30 12:45:54 -0400

    make functions more atomic and better naming

--- src/scripts/element.ts
+++ src/scripts/element.ts
@@ -1,22 +1,27 @@
-import fetch from 'sync-fetch';
+import fetch from "sync-fetch";
 
-export function splitAtomicSymbols(input: string) {
+export function removeNumbers(input: string) {
   const atomicSymbolsWithNumbers = input.match(/[A-Z][a-z]*\d*/g) || [];
   const atomicSymbolsWithoutNumbers = atomicSymbolsWithNumbers.map((token) =>
     token.replace(/\d/g, "")
   );
   return atomicSymbolsWithoutNumbers;
 }
-export function getElementSymbols() {
+export function getElementData() {
   const response = fetch(
     "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json"
   );
   const data = response.json();
+  return data;
+}
+export function processSymbols(data: any) {
   const elementSymbols = data.elements.map((element: any) => element.symbol);
   return elementSymbols;
 }
-
-export function filterValidElements(obj: { [key: string]: number }, keysArray: string[]): { [key: string]: number } {
+export function filterValidElements(
+  obj: { [key: string]: number }, // this would be the element counts object
+  keysArray: string[] // Array with valid element symbols
+): { [key: string]: number } {
   const result: { [key: string]: number } = {};
   for (const key of keysArray) {
     if (obj.hasOwnProperty(key)) {
@@ -51,47 +56,51 @@ export function convertStringsToInt(arr: string[]): (string | number)[] {
   return result;
 }
 
-export function evaluateElementCounts(tokens: (string | number)[]): { [key: string]: number } {
+export function evaluateElementCounts(tokens: (string | number)[]): {
+  [key: string]: number;
+} {
   const elementCounts: { [key: string]: number } = {};
   const stack: number[] = [1];
-  if (typeof tokens[0] === 'number') {
+  if (typeof tokens[0] === "number") {
     return elementCounts;
   }
   let i = 0;
   while (i < tokens.length) {
     const token = tokens[i];
 
-    if (typeof token === 'string' && /[A-Z]/.test(token)) {
+    if (typeof token === "string" && /[A-Z]/.test(token)) {
       const currentElement = token;
       i += 1;
       let currentMultiplier = 1;
-      if (i < tokens.length && typeof tokens[i] === 'number') {
+      if (i < tokens.length && typeof tokens[i] === "number") {
         currentMultiplier = tokens[i] as number;
         i += 1;
       }
-      elementCounts[currentElement] = (elementCounts[currentElement] || 0) + currentMultiplier * stack[stack.length - 1];
-
-    } else if (token === '(') {
+      elementCounts[currentElement] =
+        (elementCounts[currentElement] || 0) +
+        currentMultiplier * stack[stack.length - 1];
+    } else if (token === "(") {
       i += 1;
       const subFormula: (string | number)[] = [];
-      while (i < tokens.length && tokens[i] !== ')') {
+      while (i < tokens.length && tokens[i] !== ")") {
         subFormula.push(tokens[i]);
         i += 1;
       }
       i += 1;
       let currentMultiplier = 1;
-      if (i < tokens.length && typeof tokens[i] === 'number') {
+      if (i < tokens.length && typeof tokens[i] === "number") {
         currentMultiplier = tokens[i] as number;
         i += 1;
       }
       const subElementCounts = evaluateElementCounts(subFormula);
       for (const [element, count] of Object.entries(subElementCounts)) {
-        elementCounts[element] = (elementCounts[element] || 0) + count * currentMultiplier * stack[stack.length - 1];
+        elementCounts[element] =
+          (elementCounts[element] || 0) +
+          count * currentMultiplier * stack[stack.length - 1];
       }
-
-    } else if (token === ')') {
+    } else if (token === ")") {
       i += 1;
-      if (i < tokens.length && typeof tokens[i] === 'number') {
+      if (i < tokens.length && typeof tokens[i] === "number") {
         stack.pop();
         stack.push(tokens[i] as number);
         i += 1;
@@ -103,4 +112,4 @@ export function evaluateElementCounts(tokens: (string | number)[]): { [key: stri
   return elementCounts;
 }
 
-export const validElementSymbols = getElementSymbols();
+export const validElementSymbols = processSymbols(getElementData());
