5c95d87fd67cc67bb5c1711686c7a9fa6212ea77
Tnixc <tnixxc@gmail.com>
2023-11-02 14:23:34 -0400

    Add In component to Empirical view and handle calculate event.

--- src/views/Empirical.vue
+++ src/views/Empirical.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import Nav from "../components/Nav.vue";
+import In from "../components/Empirical/In.vue";
 </script>
 
 <template>
   <main>
     <Nav />
-    
+    <In @calculate="handleCalculate" />
   </main>
 </template>
+
+<script lang="ts">
+export default {
+  components: {
+    In
+  },
+  methods: {
+    handleCalculate(data: { element: string; percentage: string }) {
+      console.log("Data from child component:", data);
+    }
+  }
+};
+</script>
