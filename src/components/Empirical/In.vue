c8502eb20a5cb5b70b0238951b7e451fc3c0269f
Tnixc <tnixxc@gmail.com>
2023-11-11 20:34:47 -0500

    Refactor Empirical component to improve user experience

--- src/components/Empirical/In.vue
+++ src/components/Empirical/In.vue
@@ -1,40 +1,49 @@
 <template>
-  <div
-    class="my-4 bg-base-200/50 p-4 rounded-xl ring-2 ring-neutral shadow-lg max-w-xs flex flex-col gap-2"
-  >
+  <div>
     <div
-      v-for="(value, index) in items"
-      :key="index"
-      v-bind:class="{ removed: removedIndices.includes(index) }"
-      class="flex gap-2 transition-all flex-1"
+      class="my-4 bg-base-200/50 p-4 rounded-xl ring-2 ring-neutral shadow-lg max-w-xs flex flex-col gap-2"
     >
-      <input
-        type="text"
-        v-model="items![index]"
-        class="input w-full"
-        placeholder="Element: Percentage"
-      />
-      <button class="btn aspect-square hover:btn-error p-0 shadow-sm" @click="removeIndex(index)">
-        <svg
-          class="fill-[currentColor] scale-75"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="#ffffff"
-          viewBox="0 0 256 256"
+      <div
+        v-for="(value, index) in items"
+        :key="index"
+        v-bind:class="{ removed: removedIndices.includes(index) }"
+        class="flex gap-2 transition-all flex-1"
+      >
+        <input
+          type="text"
+          v-model="items![index]"
+          class="input w-full"
+          placeholder="Element: Percentage"
+          @keyup.enter="addItem"
+        />
+        <button
+          class="btn aspect-square hover:btn-error p-0 shadow-sm hover:shadow-lg"
+          @click="removeIndex(index)"
         >
-          <path
-            d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"
-          ></path>
-        </svg>
-      </button>
+          <svg
+            class="fill-[currentColor] scale-75"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#ffffff"
+            viewBox="0 0 256 256"
+          >
+            <path
+              d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <button class="btn shadow-md" @click="addItem">Add</button>
     </div>
-    <button class="btn shadow-md" @click="addItem">Add</button>
-  </div>
-  <div
-    class="my-4 bg-base-200/50 p-4 rounded-xl ring-2 ring-neutral shadow-lg flex flex-col gap-2 max-w-xs"
-  >
-    <button class="btn btn-accent shadow-md" @click="calculate">Calculate</button>
+    <div
+      class="my-4 bg-base-200/50 p-4 rounded-xl ring-2 ring-neutral shadow-lg max-w-xs flex flex-col gap-2"
+    >
+      <button class="btn btn-accent shadow-md flex-grow" @click="calculate">
+        Calculate
+      </button>
+      <button class="btn btn-error" @click="removeAll">Clear all</button>
+    </div>
   </div>
 </template>
 
@@ -51,6 +60,11 @@ export default {
   },
   emits: ["calculate"],
   methods: {
+    removeAll(){
+      for (let i = 0; i < this.items!.length;i++) {
+        this.items![i] = "";
+      }
+    },
     addItem() {
       this.items!.push("");
     },
